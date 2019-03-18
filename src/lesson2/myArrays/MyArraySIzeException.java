35ceebcbd015fc1fd39ee4ed230ffe53067d01d7
fudzya <fudzya@gmail.com>
2019-03-18 15:07:49 +0300

    Сделано дз урок 2

---
+++ src/lesson2/myArrays/MyArraySIzeException.java
@@ -0,0 +1,10 @@
+package lesson2.myArrays;
+
+/**
+ * @author liva
+ */
+public class MyArraySIzeException extends Exception {
+	{
+		System.err.println("Размер двумерного массива не равен 4");
+	}
+}
