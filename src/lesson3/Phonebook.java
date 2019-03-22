f3c922deeaedb413048939f0bc2f1d7fc17fd7e8
fudzya <fudzya@gmail.com>
2019-03-22 15:49:13 +0300

    Исправлено дз урока 3 в соответствии с комментариями

--- src/lesson3/Phonebook.java
+++ src/lesson3/Phonebook.java
@@ -9,19 +9,35 @@ import java.util.*;
 public class Phonebook {
 
 	private Map<Integer, String> data = new HashMap<>();
-	private List<Integer> numbers = new ArrayList<>();
 
 	public List<Integer> get(String name) {
-		Set<Map.Entry<Integer, String>> entrySet = data.entrySet();
-		for (Map.Entry<Integer, String> pair : entrySet) {
+		if (data.isEmpty()) {
+			return Collections.emptyList();
+		}
+
+		List<Integer> numbers = new ArrayList<>();
+
+		for (Map.Entry<Integer, String> pair : data.entrySet()) {
 			if (name.equals(pair.getValue())) {
 				numbers.add(pair.getKey());
 			}
 		}
+
 		return numbers;
 	}
 
 	public void add(String name, Integer telephone) {
+
+		if (name == null || name.trim().length() == 0) {
+			{
+				throw new IllegalArgumentException("Проверьте правильность введенного имени пользователя");
+			}
+		}
+
+		if (telephone == null || telephone <= 0) {
+			throw new IllegalArgumentException("Проверьте правильность введенного телефона пользователя");
+		}
+
 		data.put(telephone, name);
 	}
 }
